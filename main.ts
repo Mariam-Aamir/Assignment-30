49e279324d798a5a8e2c99bc8d370d7da7fe53c3
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-08 22:31:15 +0500

    30th  Assignment

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+//Assignment 30
+
 // Hello Admin: Make a array of five or more usernames, including the name 'admin'.
 //  Imagine you are writing code that will print a greeting to each user after
 //  they log in to a website. 
